a8ec2c36d065987b5697c267fa7891420f8663ab
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-03 21:37:36 -0700

    Wire skeleton client, add referral middleware

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
+const referrals = require('./src/server/routes/referrals');
+const users = require('./src/server/routes/users');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world!');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, 'src/client')));
+
+app.use('/referrals', referrals);
+// app.use('/users', users);
 
 app.listen(port, () => {
   console.log(`Solar flare is running on port ${port}...`);
--- src/server/routes/referrals.js
+++ src/server/routes/referrals.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.send('Hello from the GET all referrals route');
+});
+
+module.exports = router;

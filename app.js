34b2bd4b4b933e03f0862f62c258302b3b018c07
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-03 21:38:41 -0700

    Add error handlers

--- app.js
+++ app.js
@@ -18,6 +18,38 @@ app.use('/', express.static(path.join(__dirname, 'src/client')));
 app.use('/referrals', referrals);
 // app.use('/users', users);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handlers
+
+// development error handlers
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handlers
+// no stacktrace leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 app.listen(port, () => {
   console.log(`Solar flare is running on port ${port}...`);
 });

ecde3d788ebf15ae2b16cc1a332724829682107a
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-04 13:07:39 -0700

    Add users_referrals tests

---
+++ src/server/routes/users_referrals.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const queries = require('../db/queries');
+
+// *** GET all users referrals *** //
+router.get('/', (req, res, next) => {
+
+});
+
+// *** GET single users referral by id *** //
+router.get('/:id', (req, res, next) => {
+
+});
+
+// *** POST create users referral *** //
+router.post('/', (req, res, next) => {
+
+});
+
+// *** PUT create users referral *** //
+router.put('/:id', (req, res, next) => {
+
+});
+
+// *** DELETE users referral by id *** //
+router.delete('/:id', (req, res, next) => {
+
+});
+
+module.exports = router;
---
+++ test/users_referrals.spec.js
@@ -0,0 +1,148 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const knex = require('../src/server/db/knex');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Users Referrals API routes', () => {
+
+  beforeEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() => {
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      done();
+    });
+  });
+
+  describe('GET /users_referrals ', () => {
+    it('should return all users referrals', (done) => {
+      chai.request(server)
+      .get('/users_referrals')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.eql(5);
+        res.body[0].should.have.property('user_id');
+        res.body[0].user_id.should.eql(1);
+        res.body[0].should.have.property('refferal_id');
+        res.body[0].referral_id.should.eql(5);
+        done();
+      })
+    });
+  });
+
+  describe('GET /users_referrals/:id ', () => {
+    it('should return single users referral by id', (done) => {
+      chai.request(server)
+      .get('/users_referrals/2')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('user_id');
+        res.body.usr_id.should.eql(1);
+        res.body.should.have.property('referral_id');
+        res.body.referral_id.should.eql(4);
+        done();
+      });
+    });
+  });
+
+  describe('POST /users_referrals ', () => {
+    it('should create a new user referral', (done) => {
+      chai.request(server)
+      .post('/users_referrals')
+      .send({
+        user_id: 6,
+        referral_id: 6
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.should.be.a('object');
+        res.body.should.have.property('user_id');
+        res.body.user_id.should.eql(6)
+        res.body.should.have.property('referral_id');
+        res.body.referral_id.should.eql(6);
+        done();
+      });
+    });
+  });
+
+  describe('PUT /users_referrals/:id ', () => {
+    it('should update a users referral by id', (done) => {
+      chai.request(server)
+      .put('/users_referrals/2')
+      .send({
+        user_id: 2,
+        referral_id: 3
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.should.be.a('object');
+        res.body.should.have.property('user_id');
+        res.body.user_id.should.eql(2);
+        res.body.should.have.property('referral_id');
+        res.body.referral_id.should.eql(3);
+        done();
+      });
+    });
+    it('should NOT update a users referral if the id field is part of the request', (done) => {
+      chai.request(server)
+      .put('/users_referrals/2')
+      .send({
+        id: 20
+      })
+      .end((err, res) => {
+        res.should.have.status(422);
+        res.should.be.json;  // jshint ignore:line
+        res.body.should.be.a('object');
+        res.body.should.have.property('error');
+        res.body.error.should.eql('You cannot update the id field');
+        done();
+      });
+    });
+  });
+
+  describe('DELETE /users_referrals/:id ', () => {
+    it('should delete a users referral by id', (done) => {
+      chai.request(server)
+      .delete('/users_referrals/2')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        chai.request(server)
+        .get('/users_referrals')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.eql(4);
+          done();
+        });
+      });
+    });
+  });
+
+});

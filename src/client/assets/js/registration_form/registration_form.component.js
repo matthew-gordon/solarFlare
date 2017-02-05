df33915462d7cc65145a8930eb66a66b4827b964
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-04 17:43:15 -0700

    Add skeleton CSS, wire skeletion form component

---
+++ src/client/assets/js/registration_form/registration_form.component.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  angular.module('app')
+    .component('registrationForm', {
+      templateUrl: './assets/js/registration_form/registration_form.template.html'
+    });
+    function RegistrationFormController() {
+      let vm = this;
+    }
+}());

30474771d3edcfcc8db1b3f90f6916655142ddbc
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-05 15:32:59 -0700

    Add seed data

--- src/server/db/seeds/production/01_users_seed.js
+++ src/server/db/seeds/production/01_users_seed.js
@@ -6,31 +6,36 @@ exports.seed = function(knex, Promise) {
       return knex('users').insert({
         id: 1,
         name: 'chris',
-        email: 'chris@email.com'
+        email: 'chris@email.com',
+        password: 'password'
       });
     }).then(function () {
       return knex('users').insert({
         id: 2,
         name: 'ari',
-        email: 'ari@email.com'
+        email: 'ari@email.com',
+        password: 'password'
       });
     }).then(function () {
       return knex('users').insert({
         id: 3,
         name: 'matt',
-        email: 'matt@email.com'
+        email: 'matt@email.com',
+        password: 'password'
       });
     }).then(function () {
       return knex('users').insert({
         id: 4,
         name: 'liz',
-        email: 'liz@email.com'
+        email: 'liz@email.com',
+        password: 'password'
       });
     }).then(function () {
       return knex('users').insert({
         id: 5,
         name: 'laura',
-        email: 'laura@email.com'
+        email: 'laura@email.com',
+        password: 'password'
       });
     }).then(() => {
       return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
